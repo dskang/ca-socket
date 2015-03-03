5f7aca3594de46c6bc0d37923e7f64e7d00c93c2
Dan Kang <dan@dskang.com>
2015-03-03 17:59:17 -0500

    Add redis module

---
+++ server/redis.js
@@ -0,0 +1,13 @@
+var redis = require("redis");
+var url = require("url");
+
+var client;
+if (process.env.REDISTOGO_URL) {
+  var rtg = url.parse(process.env.REDISTOGO_URL);
+  client = redis.createClient(rtg.port, rtg.hostname);
+  client.auth(rtg.auth.split(":")[1]);
+} else {
+  client = redis.createClient();
+}
+
+module.exports = client;
